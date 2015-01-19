69e207e08fe29ad1e333e4a6cde4f8c3464a3652
MANU SINGHAL <singhalmanu9@gmail.com>
2015-01-18 20:26:23 -0500

    Added logger, again

---
+++ src/org/usfirst/frc/team1014/robot/Logger.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team1014.robot;
+
+
+/**
+ * Sets up the logging for the robot.
+ * @author Manu S.
+ *
+ */
+public interface Logger {
+
+	public static void log(String out){}
+}
