c50c2dd5c6fad6ed37f849db7074664a5f9a89f2
singhalmanu9 <singhalmanu9@gmail.com>
2015-02-06 20:57:03 -0500

    added parameters for easier changing

--- src/org/usfirst/frc/team1014/robot/commands/autonomous/DriveSquare.java
+++ src/org/usfirst/frc/team1014/robot/commands/autonomous/DriveSquare.java
@@ -3,17 +3,17 @@ package org.usfirst.frc.team1014.robot.commands.autonomous;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class DriveSquare extends CommandGroup {
-
-	public DriveSquare()
+	
+	public DriveSquare(double timeDrive, double speedDrive, double degree)
 	{
-		this.addSequential(new DriveStraightForward(500000, .5));
-		this.addSequential(new AutoTurn(90));
-		this.addSequential(new DriveStraightForward(500000, .5));
-		this.addSequential(new AutoTurn(90));
-		this.addSequential(new DriveStraightForward(500000, .5));
-		this.addSequential(new AutoTurn(90));
-		this.addSequential(new DriveStraightForward(500000, .5));
-		this.addSequential(new AutoTurn(90));
+		this.addSequential(new DriveStraightForward(timeDrive, speedDrive));
+		this.addSequential(new AutoTurn(degree));
+		this.addSequential(new DriveStraightForward(timeDrive, speedDrive));
+		this.addSequential(new AutoTurn(degree));
+		this.addSequential(new DriveStraightForward(timeDrive, speedDrive));
+		this.addSequential(new AutoTurn(degree));
+		this.addSequential(new DriveStraightForward(timeDrive, speedDrive));
+		this.addSequential(new AutoTurn(degree));
 	}
 
 }
